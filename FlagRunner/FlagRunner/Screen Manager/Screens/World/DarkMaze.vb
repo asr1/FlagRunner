6bffdaa0812eee1840ba34a2f400dc480ff223b2
asr1 <alexandersrinehart@gmail.com>
2014-10-31 15:57:58 -0500

    We've got lighting

--- FlagRunner/FlagRunner/Screen Manager/Screens/World/DarkMaze.vb
+++ FlagRunner/FlagRunner/Screen Manager/Screens/World/DarkMaze.vb
@@ -16,10 +16,21 @@ Public Class DarkMaze
         light.Angle = 250
         light.Position = New Vector2(0, 0)
         light.IsOn = True
+
+        Globals.KrypEng.Lights.Add(light)
+    End Sub
+
+    Public Sub CreateHulls()
+        Dim hull = ShadowHull.CreateRectangle(New Vector2(1, 1))
+        hull.Position.X = 10
+        hull.Position.Y = 10
+
+        Globals.KrypEng.Hulls.Add(hull)
     End Sub
 
     Public Overrides Sub Draw()
-        MyBase.Draw()
+  
+
         Dim World As Matrix = Matrix.Identity
         Dim View As Matrix = Matrix.CreateTranslation(New Vector3(0, 0, 0) * -1)
         Dim Projection As Matrix = Matrix.CreateOrthographic(Globals.GameSize.Y * Globals.Graphics.GraphicsDevice.Viewport.AspectRatio, Globals.GameSize.Y, 0, 1)
@@ -28,13 +39,27 @@ Public Class DarkMaze
         Globals.KrypEng.Bluriness = 3
         Globals.KrypEng.LightMapPrepare()
 
-        Globals.Graphics.GraphicsDevice.Clear(Color.Black)
+
+        createLights()
+        CreateHulls()
+        MyBase.Draw()
 
 
+        Globals.KrypEng.Draw(Globals.GameTime)
+        For Each l As Light2D In Globals.KrypEng.Lights
+            Globals.KrypEng.RenderHelper.BufferAddBoundOutline(l.Bounds)
 
-        Globals.SpriteBatch.Begin()
+        Next
+
+
+        'Globals.Graphics.GraphicsDevice.Clear(Color.White)
+        'Globals.Graphics.GraphicsDevice.SetRenderTarget(Nothing)
+        'Globals.Graphics.GraphicsDevice.Present()
+
+
+        '     Globals.SpriteBatch.Begin()
         'Globals.SpriteBatch.Draw(light, New Rectangle(0, 0, 10, 10), Color.White)
-        Globals.SpriteBatch.End()
+        '   Globals.SpriteBatch.End()
     End Sub
 
 End Class
